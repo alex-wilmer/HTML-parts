a422655847d193eb6e8f315c7baf05a95b5b1c55
Alex Wilmer <dhazium@gmail.com>
2014-07-03 02:56:20 -0400

    wi icons

--- weatherFeed/api.js
+++ weatherFeed/api.js
@@ -3,28 +3,29 @@ var ngAPI = angular.module('ngAPI', []);
 ngAPI.controller('APIController', function ($scope, $http) {
 	$scope.date = new Date();
  	$scope.city = 'Toronto';
- 	$scope.getWeather = function (city) {
+ 	$http.get('codes.json').success(function(api){$scope.codes = api.codes;});
+ 	$scope.getForecast = function (city) {
 	  	$http
 	  	.get('https://api.worldweatheronline.com/free/v1/weather.ashx?q=' 
 	  		+ $scope.city + 
 	  		'&format=json&num_of_days=5&key=707a8a94e3eb000ed78b12dff3007be26732de7c')
 	  	.success(function(api) {
-	  	$scope.days = [];
-	  	for (var i=1;i<5;i++) {
-	  		$scope.days[i] = {
-	  			name: $scope.getDay($scope.date.getDay()+i),
-	  			c: Math.floor(avgTemp(
-		  				api.data.weather[i].tempMinC, 
-		  				api.data.weather[i].tempMaxC)),
-	  			f: Math.floor(avgTemp(
-				  		api.data.weather[i].tempMinF, 
-				  		api.data.weather[i].tempMaxF))
-	  		}
-	  	}
-	    $scope.api = api.data;
+		  	$scope.days = [];
+		  	for (var i=1;i<5;i++) {
+		  		$scope.days[i] = {
+		  			name: $scope.getDay($scope.date.getDay()+i),
+		  			c: Math.floor(avgTemp(
+			  				api.data.weather[i].tempMinC, 
+			  				api.data.weather[i].tempMaxC)),
+		  			f: Math.floor(avgTemp(
+					  		api.data.weather[i].tempMinF, 
+					  		api.data.weather[i].tempMaxF))
+		  		}
+		  	}
+		    $scope.api = api.data;
 	    });
 	}
-	$scope.getWeather($scope.city);
+	$scope.getForecast($scope.city);
 
 	//UTILITY
 	$scope.number = 3;
@@ -45,5 +46,20 @@ ngAPI.controller('APIController', function ($scope, $http) {
 		if (number == 4) { return 'Thursday'; }
 		if (number == 5) { return 'Friday'; }
 		if (number == 6) { return 'Saturday'; }
+	}
+
+	$scope.getMonth = function (number) {
+		if (number == 0) { return 'January'; }
+		if (number == 1) { return 'February'; }
+		if (number == 2) { return 'March'; }
+		if (number == 3) { return 'April'; }
+		if (number == 4) { return 'May'; }
+		if (number == 5) { return 'June'; }
+		if (number == 6) { return 'July'; }
+		if (number == 7) { return 'August'; }
+		if (number == 8) { return 'September'; }
+		if (number == 9) { return 'October'; }
+		if (number == 10) { return 'November'; }
+		if (number == 11) { return 'December'; }
 	}
 });
